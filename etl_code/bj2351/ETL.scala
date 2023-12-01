28bc4af0f5d3f612989275fa41fcce2d4ccf8008
bonjune <bongjun.jang@kaist.ac.kr>
2023-12-01 18:46:39 +0000

    done weather ETL

--- etl_code/bj2351/ETL.scala
+++ etl_code/bj2351/ETL.scala
@@ -1,92 +1,55 @@
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions._
 
-val inputFile = "hw7/central-park-2022-weather.csv"
-val weatherDF = spark.read.option("header", true).csv(inputFile)
+val ingestionPath = "/user/bj2351_nyu_edu/final/data/"
+val inputFile = ingestionPath + "hourly-weather-nyc-2022.csv"
 
-val usingColumns =
-      Array(
-          "DATE",
-          "ELEVATION",
-          "REPORT_TYPE",
-          "SOURCE",
-          "HourlyAltimeterSetting",
-          "HourlyDewPointTemperature",
-          "HourlyDryBulbTemperature",
-          "HourlyPrecipitation",
-          "HourlyPresentWeatherType",
-          "HourlyPressureChange",
-          "HourlyPressureTendency",
-          "HourlyRelativeHumidity",
-          "HourlySkyConditions",
-          "HourlySeaLevelPressure",
-          "HourlyStationPressure",
-          "HourlyVisibility",
-          "HourlyWetBulbTemperature",
-          "HourlyWindDirection",
-          "HourlyWindGustSpeed",
-          "HourlyWindSpeed",
-          "Sunrise",
-          "Sunset"
-      )
+val df: DataFrame = spark.read.option("header", true).csv(inputFile)
 
-def shrinkColumns(df: DataFrame, colsInUse: Seq[String]): DataFrame = {
-    val cols = df.columns
-    val dropCols = cols.toSet -- colsInUse.toSet
-    val droppedDf = df.drop(dropCols.toSeq: _*)
-    droppedDf
-}
+// The STATION column is not needed.
+// We only collect data from the Central Park.
 
-val droppedWeatherDF = shrinkColumns(weatherDF, usingColumns)
+val weatherDF: DataFrame = df.drop("STATION")
 
-droppedWeatherDF.printSchema()
+// 1. select useful columns for each report type
+val fm15 = weatherDF
+  .filter('REPORT_TYPE === "FM-15")
+  .drop("REPORT_TYPE", "DailyWeather", "Sunrise", "Sunset")
 
-val fm15 = droppedWeatherDF.filter('REPORT_TYPE === "FM-15").drop("REPORT_TYPE")
-val fm16 = droppedWeatherDF.filter('REPORT_TYPE === "FM-16").drop("REPORT_TYPE")
-val sod = droppedWeatherDF.filter('REPORT_TYPE === "SOD  ").drop("REPORT_TYPE")
-val som = droppedWeatherDF.filter('REPORT_TYPE === "SOM  ").drop("REPORT_TYPE")
+val fm16 = weatherDF
+  .filter('REPORT_TYPE === "FM-16")
+  .drop("REPORT_TYPE", "DailyWeather", "Sunrise", "Sunset")
 
-def countNulls(df: DataFrame) {
-    println(df.count() + " rows in total")
-    val cols = df.columns
-    cols.foreach(col => {
-        val col_nulls = (col, df.filter(df(col).isNull).count())
-        println(col_nulls)
-    })
-}
+val sod  = weatherDF
+  .filter('REPORT_TYPE === "SOD  ")
+  .drop("REPORT_TYPE")
+  .select("DATE", "SOURCE", "DailyWeather", "Sunrise", "Sunset")
 
-println("FM-15 reports null counts")
-countNulls(fm15)
+// 2. Processing SOD (adding boolean columns)
+val sod1 = sod.withColumn("Rain", col("DailyWeather").contains("RA")).na.fill(Map("Rain" -> false))
+val sod2 = sod1.withColumn("Snow", col("DailyWeather").contains("SN")).na.fill(Map("Snow" -> false))
+val sod3 = sod2.withColumn("Mist", col("DailyWeather").contains("BR")).na.fill(Map("Mist" -> false))
+val sod4 = sod3.withColumn("Haze", col("DailyWeather").contains("HZ")).na.fill(Map("Haze" -> false))
+val sod5 = sod4.withColumn("Fog", col("DailyWeather").contains("FG")).na.fill(Map("Fog" -> false))
+val sodWithB = sod5.withColumn("date_only", to_date(col("DATE"))).orderBy("date_only").drop("SOURCE", "DATE")
+val sodResult = sodWithB.select("date_only", "Sunrise", "Sunset", "Rain", "Snow", "Mist", "Haze", "Fog")
+sodResult.show(5)
 
-println("FM-15 has only null values in Sunrise and Sunset columns")
-val cleanFm15: DataFrame = fm15.drop("Sunrise", "Sunset")
+// 3. Processing FM15 (Hourly Weather Data)
+// Need min/max temperature per day
 
-println("FM-16 reports null counts")
-countNulls(fm16)
-println("FM-16 has only null values in HourlyPressureChange, HourlyPressureTendency, Sunrise and Sunset columns")
-val cleanFm16: DataFrame = fm16.drop(
-    "HourlyPressureChange",
-    "HourlyPressureTendency",
-    "Sunrise", "Sunset")
+val profTemp = fm15
+  .withColumn("date_only", to_date(col("DATE")))
+  .groupBy("date_only")
+  .agg(
+    min("HourlyDryBulbTemperature").alias("MinTemp"),
+    max("HourlyDryBulbTemperature").alias("MaxTemp"),
+    avg("HourlyDryBulbTemperature").alias("AvgTemp")
+  )
+  .orderBy("date_only")
 
-println("SOD reports null counts")
-countNulls(sod)
+profTemp.show(5)
 
-println("For SOD, we only non-null values for Sunrise and Sunset columns")
-val cleanSod = shrinkColumns(sod, Array(
-    "DATE",
-    "ELEVATION",
-    "SOURCE",
-    "Sunrise",
-    "Sunset"
-))
-
-println("SOM reports null counts")
-countNulls(som)
-
-println("For SOM, we only have null values for all columns")
-println("Don't use SOM")
-
-cleanFm15.coalesce(1).write.option("header", true).mode("overwrite").csv("final/fm-15")
-cleanFm16.coalesce(1).write.option("header", true).mode("overwrite").csv("final/fm-16")
-cleanSod.coalesce(1).write.option("header", true).mode("overwrite").csv("final/sod")
+profTemp.coalesce(1).write.option("header", true).mode("overwrite").csv("final/cleaned/weather/agg")
+sodResult.coalesce(1).write.option("header", true).mode("overwrite").csv("final/cleaned/weather/cond")
 
